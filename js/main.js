5d08e20805c8ec04777737941ce651c17b5449f3
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-12-15 15:50:27 +0100

    vue-slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,38 @@ Bonus ufficiale:
 Facciamo in modo che il nostro slider scorra automaticamente in avanti,
 e ogni 3 secondi visualizzi un immagine differente.
 Tip: possiamo usare una lifecycle Hook per inserire un nostro script quando l'istanza Vue é stata caricata */
+
+
+
+let app = new Vue ({
+    el: "#app",
+    data: {
+        counter: 0,
+        images: [
+            "img/nature.jpg",
+            "img/scogli.jpg",
+            "img/tramonto.jpg",
+            "img/vista.jpg",
+        ],
+    },
+    created: function(){
+        setInterval(this.prev, 3000);
+        
+    },
+    methods: {
+        next() {
+            this.counter++;
+            if(this.counter === this.images.length) {
+                this.counter = 0;
+            }
+        },
+        prev() {
+            this.counter--;
+            if (this.counter === -1){
+                this.counter = this.images.length -1 ;
+            }
+            
+        },
+        
+    }
+})
