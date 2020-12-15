3123d79386e2f77fb3464e177934732ec51a1cdb
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-12-15 13:06:47 +0100

    vue-slider inizio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/* Istruzioni:
+Ricreare lo slider di immagini ma questa volta con Vue
+Bonus ufficiale:
+Facciamo in modo che il nostro slider scorra automaticamente in avanti,
+e ogni 3 secondi visualizzi un immagine differente.
+Tip: possiamo usare una lifecycle Hook per inserire un nostro script quando l'istanza Vue é stata caricata */
